0630df9f8834279d75a71f1479289268baaa5507
彭康 <pk@minizhen.com>
2016-11-29 18:13:05 +0800

    修改

--- index.php
+++ index.php
@@ -39,40 +39,40 @@
      * 邮件主题：【微信JS-SDK反馈】具体问题
      * 邮件内容说明：用简明的语言描述问题所在，并交代清楚遇到该问题的场景，可附上截屏图片，微信团队会尽快处理你的反馈。
      */
+Zepto(function($){
 
-
-wx.config({
-          debug: false,
-          appId: '<?php echo $signPackage["appId"];?>',
-          timestamp: '<?php echo $signPackage["timestamp"];?>',
-          nonceStr: '<?php echo $signPackage["nonceStr"];?>',
-          signature: '<?php echo $signPackage["signature"];?>',
-          jsApiList: [
-	                      // 所有要调用的 API 都要加到这个列表中
-                          'checkJsApi',
-                          'openLocation',
-                          'getLocation',
-                          'onMenuShareTimeline',
-                          'onMenuShareAppMessage'
-                      ]
-});
-wx.ready(function () {
-       // 在这里调用 API
-	wx.getLocation({
-	    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-	    success: function (res) {
-	        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-	        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-	        var speed = res.speed; // 速度，以米/每秒计
-	        var accuracy = res.accuracy; // 位置精度
-	        alert(latitude);
-	        alert(longitude);
-	        alert(speed);
-	        alert(accuracy);
-	    }
-	});                      
-});
-
+        wx.config({
+                  debug: false,
+                  appId: '<?php echo $signPackage["appId"];?>',
+                  timestamp: '<?php echo $signPackage["timestamp"];?>',
+                  nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+                  signature: '<?php echo $signPackage["signature"];?>',
+                  jsApiList: [
+        	                      // 所有要调用的 API 都要加到这个列表中
+                                  'checkJsApi',
+                                  'openLocation',
+                                  'getLocation',
+                                  'onMenuShareTimeline',
+                                  'onMenuShareAppMessage'
+                              ]
+        });
+        wx.ready(function () {
+               // 在这里调用 API
+        	wx.getLocation({
+        	    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+        	    success: function (res) {
+        	        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+        	        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+        	        var speed = res.speed; // 速度，以米/每秒计
+        	        var accuracy = res.accuracy; // 位置精度
+        	        alert(latitude);
+        	        alert(longitude);
+        	        alert(speed);
+        	        alert(accuracy);
+        	    }
+        	});                      
+        });
+})
 </script>  
 
 </body>
