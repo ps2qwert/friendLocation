b6c7425f9e119c2ad45321c89e9c76e69f314866
彭康 <pk@minizhen.com>
2016-11-29 18:35:23 +0800

    update

--- index.php
+++ index.php
@@ -40,11 +40,10 @@
      * 邮件内容说明：用简明的语言描述问题所在，并交代清楚遇到该问题的场景，可附上截屏图片，微信团队会尽快处理你的反馈。
      */
 Zepto(function($){
-
         wx.config({
                   debug: false,
                   appId: '<?php echo $signPackage["appId"];?>',
-                  timestamp: '<?php echo $signPackage["timestamp"];?>',
+                  timestamp: <?php echo $signPackage["timestamp"];?>,
                   nonceStr: '<?php echo $signPackage["nonceStr"];?>',
                   signature: '<?php echo $signPackage["signature"];?>',
                   jsApiList: [
